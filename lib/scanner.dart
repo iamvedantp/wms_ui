945041ee7ad48c66a089193b0d8de36d794d4eed
iamvedantp <vedantp183@gmail.com>
2024-09-19 18:58:29 +0530

    Qr/Barcode scanner added

---
+++ lib/scanner.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class Scanner extends StatefulWidget {
+  @override
+  _ScannerState createState() => _ScannerState();
+}
+
+class _ScannerState extends State<Scanner> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  QRViewController? controller;
+  Barcode? result;
+  bool flashOn = false;
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (controller != null) {
+      controller!.pauseCamera();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('QR/Barcode Scanner'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: _onQRViewCreated,
+              overlay: QrScannerOverlayShape(
+                borderColor: Colors.red,
+                borderRadius: 10,
+                borderLength: 30,
+                borderWidth: 10,
+                cutOutSize: 300,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                Center(
+                  child: (result != null)
+                      ? Text(
+                          'Barcode Type: ${result!.format}   Data: ${result!.code}')
+                      : const Text('Scan a code'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      flashOn = !flashOn;
+                      controller?.toggleFlash();
+                    });
+                  },
+                  child: Text(flashOn ? 'Turn Flash Off' : 'Turn Flash On'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+}
