5d57f20ab902e865ba2bab5eea081762cd57e6d1
iamvedantp <vedantp183@gmail.com>
2024-09-25 17:17:05 +0530

    route return page functions updated with required functions

--- lib/Functions/Operator Functions/Picklists/picklists_function_page.dart
+++ lib/Functions/Operator Functions/Picklists/picklists_function_page.dart
@@ -1,3 +1,5 @@
+import 'package:code_practice/Functions/Operator%20Functions/Picklists/check_in_item.dart';
+import 'package:code_practice/Functions/Operator%20Functions/Picklists/check_in_pallet.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -6,7 +8,7 @@ class RouteReturnPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    //Defined the button style once
+    // Define the button style once
     final ButtonStyle outlinedButtonStyle =
         OutlinedButton.styleFrom(shape: const LinearBorder());
     return Scaffold(
@@ -19,18 +21,28 @@ class RouteReturnPage extends StatelessWidget {
         children: [
           OutlinedButton.icon(
             iconAlignment: IconAlignment.start,
-            icon: const FaIcon(Icons.camera_enhance, size: 50),
+            icon: const FaIcon(Icons.check_box_outlined, size: 50),
             style: outlinedButtonStyle,
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CheckInItemPage()),
+              );
+            },
             label: const Text('Check-In Item'),
           ),
           OutlinedButton.icon(
             iconAlignment: IconAlignment.start,
-            icon: const FaIcon(Icons.camera_enhance, size: 50),
+            icon: const FaIcon(Icons.account_balance_wallet, size: 50),
             style: outlinedButtonStyle,
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => CheckInPalletPage()),
+              );
+            },
             label: const Text('Check-In Pallet'),
-          )
+          ),
         ],
       ),
     );
