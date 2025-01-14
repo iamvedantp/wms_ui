f933e80e58da89e08d14256683b436572ce1dd0d
iamvedantp <vedantp183@gmail.com>
2025-01-14 22:41:34 +0530

    Scanner Deactivated temporarily

--- lib/Functions/Scanner/scanner.dart
+++ lib/Functions/Scanner/scanner.dart
@@ -1,92 +1,92 @@
-import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+// import 'package:flutter/material.dart';
+// import 'package:qr_code_scanner/qr_code_scanner.dart';
 
-class Scanner extends StatefulWidget {
-  const Scanner({super.key});
+// class Scanner extends StatefulWidget {
+//   const Scanner({super.key});
 
-  @override
-  ScannerState createState() => ScannerState();
-}
+//   @override
+//   ScannerState createState() => ScannerState();
+// }
 
-class ScannerState extends State<Scanner> {
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-  QRViewController? controller;
-  Barcode? result;
-  bool flashOn = false;
+// class ScannerState extends State<Scanner> {
+//   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+//   QRViewController? controller;
+//   Barcode? result;
+//   bool flashOn = false;
 
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (controller != null) {
-      controller!.pauseCamera();
-    }
-  }
+//   @override
+//   void reassemble() {
+//     super.reassemble();
+//     if (controller != null) {
+//       controller!.pauseCamera();
+//     }
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('QR/Barcode Scanner'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            flex: 5,
-            child: QRView(
-              key: qrKey,
-              onQRViewCreated: _onQRViewCreated,
-              overlay: QrScannerOverlayShape(
-                borderColor: Colors.red,
-                borderRadius: 10,
-                borderLength: 30,
-                borderWidth: 10,
-                cutOutSize: 300,
-              ),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Column(
-              children: [
-                Center(
-                  child: (result != null)
-                      ? Text(
-                          'Barcode Type: ${result!.format}   Data: ${result!.code}')
-                      : const Text('Scan a code'),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        flashOn = !flashOn;
-                        controller?.toggleFlash();
-                      });
-                    },
-                    child: Icon(
-                      flashOn ? Icons.flash_on : Icons.flash_off,
-                      size: 30,
-                    )
-                    // Text(flashOn ? 'Turn Flash Off' : 'Turn Flash On'),
-                    ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('QR/Barcode Scanner'),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             flex: 5,
+//             child: QRView(
+//               key: qrKey,
+//               onQRViewCreated: _onQRViewCreated,
+//               overlay: QrScannerOverlayShape(
+//                 borderColor: Colors.red,
+//                 borderRadius: 10,
+//                 borderLength: 30,
+//                 borderWidth: 10,
+//                 cutOutSize: 300,
+//               ),
+//             ),
+//           ),
+//           Expanded(
+//             flex: 1,
+//             child: Column(
+//               children: [
+//                 Center(
+//                   child: (result != null)
+//                       ? Text(
+//                           'Barcode Type: ${result!.format}   Data: ${result!.code}')
+//                       : const Text('Scan a code'),
+//                 ),
+//                 ElevatedButton(
+//                     onPressed: () {
+//                       setState(() {
+//                         flashOn = !flashOn;
+//                         controller?.toggleFlash();
+//                       });
+//                     },
+//                     child: Icon(
+//                       flashOn ? Icons.flash_on : Icons.flash_off,
+//                       size: 30,
+//                     )
+//                     // Text(flashOn ? 'Turn Flash Off' : 'Turn Flash On'),
+//                     ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
 
-  void _onQRViewCreated(QRViewController controller) {
-    this.controller = controller;
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
-    });
-  }
+//   void _onQRViewCreated(QRViewController controller) {
+//     this.controller = controller;
+//     controller.scannedDataStream.listen((scanData) {
+//       setState(() {
+//         result = scanData;
+//       });
+//     });
+//   }
 
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-}
+//   @override
+//   void dispose() {
+//     controller?.dispose();
+//     super.dispose();
+//   }
+// }
